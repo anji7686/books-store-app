4b97741cdf4ad24346ca668234abf50f4d4ea35f
Anjaneyulu <anjaneyulu@kreatio.com>
2015-05-06 14:49:38 +0530

    books initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root 'main#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  mount Lockup::Engine, at: '/lockup'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'pages#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
